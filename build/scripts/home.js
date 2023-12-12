e1db4599ba45132766ee6d71a371b1f2ebd4079e
Daniel <96059363+DanielUretsky@users.noreply.github.com>
2023-12-12 21:06:55 +0200

    Update home.js

--- build/scripts/home.js
+++ build/scripts/home.js
@@ -2,13 +2,3 @@
 console.log("hello");
 console.log("sads");
 console.log("sads5");
-
-console.log("sads5");
-
-console.log("sads5");
-console.log("sads5");
-console.log("sads5");
-console.log("sads5");
-console.log("sads5");
-console.log("sads6");
-"57961bbc0c702967c76c0691762bebcc28109f7d"
