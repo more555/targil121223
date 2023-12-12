3d0dddd6f447a7c32bae5c944d1eb265c1bd454f
Daniel <daniel353a3@gmail.com>
2023-12-12 21:05:03 +0200

    build files and folders

--- build/scripts/home.js
+++ build/scripts/home.js
@@ -2,12 +2,6 @@
 console.log("hello");
 console.log("sads");
 console.log("sads5");
-<<<<<<< HEAD
-=======
-console.log("sads5");
-console.log("sads5");
-console.log("sads5");
-console.log("sads5");
-console.log("sads5");
-console.log("sads6");
->>>>>>> 57961bbc0c702967c76c0691762bebcc28109f7d
+
+
+
